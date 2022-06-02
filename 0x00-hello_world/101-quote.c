3c17dff0e3efeed90346cc8b152fd8b879568042
yikunoamlak <105526101+yikunoamlak@users.noreply.github.com>
2022-06-02 22:41:19 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - Entry point
- * Return:1
+ *main - entry point
+ *
+ *Return: value (1)
  */
 int main(void)
 {
-char ch[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
-write(2, ch, sizeof(ch));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
+
 }
