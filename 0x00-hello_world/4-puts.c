7fdf22de0a006814a1e53b111575363b11277955
yikunoamlak <yikunkala13@gmail.com>
2022-06-02 12:11:47 -0700

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 /**
-* main - Entry point of the program
-*
-* Return - Always returns (0)
-*/
+ * main -Entry point of the program
+ * 
+ * Return - Always returns (0)
+ *
+ * Description:  A program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line
+ * 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
