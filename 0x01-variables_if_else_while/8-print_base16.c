93fe386c40609b9e8d91f48c65c6052cae1c93af
yikunoamlak <105526101+yikunoamlak@users.noreply.github.com>
2022-06-04 02:19:28 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,17 +6,24 @@
  * Description: prints all the numbers of base 16 in lowercase
  * Return: 0
  */
-int main (void)
+int main(void)
 {
-  char c= '0';
+	char d = '0';
 
-  while (c <='9')
-    {
-      putchar (c);
-      c++;
-    }
-  
-  putchar('\n');
-  return (0);
-    
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+
+	char c = 'a';
+
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
 }
