ac19a90d395f7f390fa4ed3b39a1e61997d43c52
yikunoamlak <105526101+yikunoamlak@users.noreply.github.com>
2022-06-04 01:49:40 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,15 +8,14 @@
 
 int main(void)
 {
-  int c=0;
+	int c = 0;
 
-  while (c<10)
-    {
-      putchar(48+c);
-      c++;
-    }
-  
-  putchar('\n');
-  return(0);
-  
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
 }
