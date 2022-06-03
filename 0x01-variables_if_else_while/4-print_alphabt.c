7c32af4eb2348a74c12a95b29747374db42e6cd7
yikunoamlak <105526101+yikunoamlak@users.noreply.github.com>
2022-06-04 00:57:54 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,15 +8,15 @@
 
 int main(void)
 {
-char c = 'a';
-  while (c <= 'z')
-  {
-      if ( c != 'e' && c != 'q')
+	char c = 'a';
+	while (c <= 'z')
+{
+	if (c != 'e' && c != 'q')
 	{
-	putchar(c); 
+	putchar(c);
 	}
 	c++; 
-    }
-  putchar('\n');
-  return (0);
+}
+	putchar('\n');
+	return (0);
 }
