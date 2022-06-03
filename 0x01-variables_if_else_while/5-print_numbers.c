be7ad2ba9e9f68d40feca2d6fa402f302e6256e7
yikunoamlak <105526101+yikunoamlak@users.noreply.github.com>
2022-06-04 01:37:25 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,10 +2,10 @@
 #include <time.h>
 #include <stdlib.h>
 /**
- * main -entry point
- * Description - prints all single digit numbers of base 10 starting from 0
- * Return -returns always (0)
- */
+* main -entry point
+* Description - prints all single digit numbers of base 10 starting from 0
+* Return -returns always (0)
+*/
 int main(void)
 {
 	char c = '0';
