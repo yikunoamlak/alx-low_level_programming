09e9843f072a333ccf3028b48534b2e86e3b3c23
yikunoamlak <yikunkala13@gmail.com>
2022-06-03 14:20:44 -0700

    Update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,7 +4,7 @@
 /**
  * main - Entry point
  * Description: Get a random number and check its last digit, compare it with 5 
- * Return: always (0)
+ * Return: returns always (0)
  */
 int main(void)
 {
