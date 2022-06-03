97ee469c3501ad67c017277b9e35c26660cb30ab
yikunoamlak <yikunkala13@gmail.com>
2022-06-03 15:59:05 -0700

    Update

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main-main entry
+ * Description - prints all the numbers of base 16 in lowercase
+ * Return : always return (0)
+ */
+
+int main(void)
+{
+  char b = 'z';
+
+  while (b >= 'a')
+    {
+      putchar(b);
+      c--;
+    }
+  putchar('\n');
+  return (0);
+}
+  
