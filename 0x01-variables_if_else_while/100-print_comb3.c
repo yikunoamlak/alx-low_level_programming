245f70527c1d0df25a67ff7a744abf0f18e4869a
yikunoamlak <105526101+yikunoamlak@users.noreply.github.com>
2022-06-04 02:43:34 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,55 +7,30 @@
  */
 int main(void)
 {
-  
-  int c;
-  
-  int d = 0;
-  
-  while (d < 10)
-    
-    {
-      
-      c = 0;
-      
-      while (c < 10)
-	
+	int c;
+	int d = 0;
+
+	while (d < 10)
 	{
-	  
-	  if (d != c && d < c)
-	    
-	    {
-	      
-	      putchar('0' + d);
-	      
-	      putchar('0' + c);
-	      
-
-	      
-	      if (c + d != 17)
-		
+		c = 0;
+		while (c < 10)
 		{
-		  
-		  putchar(',');
-		  
-		  putchar(' ');
-		  
-		}
-	      
-	    }
-	  
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
 
-	  
-	  c++;
-	  
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			c++;
+		}
+		d++;
 	}
-      
-      d++;
-      
-    }
-  
-  putchar('\n');
-  
-  return (0);
-  
+	putchar('\n');
+	return (0);
 }
