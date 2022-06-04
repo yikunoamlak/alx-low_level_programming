9569cd60c0b3ac9c91af02c26d9c9f5441ac666a
yikunoamlak <105526101+yikunoamlak@users.noreply.github.com>
2022-06-04 03:18:42 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Description - prints the alphabet in lowercase, and then in uppercase
- * Return: returns always (0)
+ * main - main entry
+ * Description: prints the alphabet in lowercase, and then in uppercase
+ * Return: 0
  */
 
 int main(void)
