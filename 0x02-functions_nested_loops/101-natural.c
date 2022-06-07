3519fb489b2a6fae8146ac1fdf3d904fa85df5f8
yikunoamlak <yikunkala13@gmail.com>
2022-06-07 22:29:01 +0000

    Updates file 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,13 +13,15 @@ int main(void)
 	int total = 0;
 	
 	while (i < 1024)
+
 	{
 		if (i % 3 == 0)
 			total += i;
 		else if (i % 5 == 0)
 			total += i;
-		i++;i
+		i++;
 	}
+
 	printf("%d\n", total);
 	return (0);
 }
