1a6128b2cac4f124767e04b1ca0fa9a2ae81fdca
yikunoamlak <yikunkala13@gmail.com>
2022-06-07 18:48:39 +0000

    Update file 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * islower - checks for lowercase character
+ * @C : single character check
+ * Return : 1 if c is lowercase else 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return(0);
+}
