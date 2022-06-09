b07e7920c69eb893b1483f41fa26c3491c27ab32
yikunoamlak <105526101+yikunoamlak@users.noreply.github.com>
2022-06-09 23:56:44 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,51 +1,26 @@
-nclude <stdio.h>
-
+#include <stdio.h>
 #include <math.h>
-
 /**
+ * main - prime numbers
  *
- *  * main - prime numbers
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
+ * Return: Always 0.
+ */
 int main(void)
-
 {
 
+unsigned int long n = 612852475143, a = (int) sqrt(n);
 
+while (1)
+{
 
-	unsigned int long n = 612852475143, a = (int) sqrt(n);
-
-
-
-	while (1)
-
+	if (n % a == 0)
 	{
-
-
-
-			if (n % a == 0)
-
-					{
-
-								printf("%lu \n", n / a);
-
-										break;
-
-											}
-
-				a--;
-
-
-
+		printf("%lu \n", n / a);
+		break;
 	}
+	a--;
 
+}
 
-
-	return (0);
-
+return (0);
 }
