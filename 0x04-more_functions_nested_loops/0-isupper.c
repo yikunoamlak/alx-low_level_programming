94cfb51540474853a4b16e766c86143264199370
yikunoamlak <yikunkala13@gmail.com>
2022-06-09 02:28:41 -0700

    Update README.md

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: checks for uppercase
+ * return:returns 1 if uppercase else 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+# C - More functions, more nested loops
+---
+| Task | File | Description |
+| --- | --- | --- |
+| 0.isupper | 0-isupper.c | A function that checks for uppercase character. |
