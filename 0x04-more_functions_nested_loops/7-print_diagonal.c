4f7a2241ef90a7ead878bf3cbc51379adbff1a96
yikunoamlak <yikunkala13@gmail.com>
2022-06-09 12:14:31 -0700

    update

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * main - print_diagonal
+ * Description: drawis diagonal line
+ * Return: 0
+ */
+
+void print_diagonal(int n);
+{
+		int i = 0, ii;
+
+
+
+			while (i < n && n > 0)
+
+					{
+
+								ii = 0;
+
+										while (ii < i)
+
+													{
+
+																	_putchar(' ');
+
+																				ii++;
+
+																						}
+
+
+
+												_putchar('\\');
+
+														_putchar('\n');
+
+																i++;
+
+																	}
+
+				if (i == 0)
+
+							_putchar('\n');
+
+
+
+}
