f872de5e2a5dd15280fb58a5a6f3a88bbcb3b5cd
yikunoamlak <yikunkala13@gmail.com>
2022-06-13 12:09:24 -0700

    Update file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints to stdout
+ *@str: pointer to string
+ * Returns: Always 0
+ */
+
+void _puts(char *str);
+{
+	int i = 0;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
