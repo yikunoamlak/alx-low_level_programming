5085343cf89f8b659a20cd2e2f687c067246b44a
yikunoamlak <yikunkala13@gmail.com>
2022-06-13 11:59:41 -0700

    Update file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen -length of a string
+ * @s: pointer to the string
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	while (s[i])
+		i++;
+
+	return (1);
+}
