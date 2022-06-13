d2ae8f86846acb896c76ff531bca23108245b8a5
yikunoamlak <yikunkala13@gmail.com>
2022-06-13 12:23:16 -0700

    Update file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * print_rev - prints in reverse
+ * @s: string
+ * Returns: Always 0
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	while (s[i])
+		i++;
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+
+}
