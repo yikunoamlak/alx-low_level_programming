777653d479e44a8971ac59440f68a29db8c0756d
yikunoamlak <105526101+yikunoamlak@users.noreply.github.com>
2022-06-16 22:45:57 +0300

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,16 +4,16 @@
  * @a: pointer
  * @n: array length
  * Return: void
- */
+*/
 
 void reverse_array(int *a, int n)
 {
-	int i = 0, half;
-	for (half = n / 2; half > 0; half--, i++)
-	{
-		a[n - i - 1] += a[i];
-		a[i] = a[n - i - 1] - a[i];
-		a[n - i - 1] = a[n - i - 1] - a[i];
-	}
+int i = 0, half;
 
+for (half = n / 2; half > 0; half--, i++)
+{
+	a[n - i - 1] += a[i];
+	a[i] = a[n - i - 1] - a[i];
+	a[n - i - 1] = a[n - i - 1] - a[i];
+}
 }
