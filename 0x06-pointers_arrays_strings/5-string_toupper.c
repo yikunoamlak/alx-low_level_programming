eb071e85515817a61fafe7d630ee81917a1bbef5
yikunoamlak <105526101+yikunoamlak@users.noreply.github.com>
2022-06-16 22:45:29 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,16 +3,15 @@
  * *string_toupper - capitalize a string
  * @str: pointer
  * Return: capitalzied string
- */
+*/
 
-char *string_toupper(char *s)
+char *string_toupper(char *str)
 {
-	int i;
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] <= 'z' && str[i] >= 'a')
-			str[i] -= 32;
-	}
-
-	return (str);
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+	if (str[i] <= 'z' && str[i] >= 'a')
+		str[i] -= 32;
+}
+return (str);
 }
