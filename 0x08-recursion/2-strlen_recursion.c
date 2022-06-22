50a6f011473fcf44af05afed2b11ee9ea33caa0e
yikunoamlak <105526101+yikunoamlak@users.noreply.github.com>
2022-06-22 22:44:54 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,11 +7,12 @@
 
 int _strlen_recursion(char *s)
 {
-	int n = 0;
-	if (*s > '\0')
-	{
-		n += _strlen_recursion(s + 1) + 1;
-	}
-	
-	return (n);
+int n = 0;
+
+if (*s > '\0')
+{
+	n += _strlen_recursion(s + 1) + 1;
+}
+
+return (n);
 }
