15d1517beb0432cf34e154ae019b074e1c7e3ff4
yikunoamlak <105526101+yikunoamlak@users.noreply.github.com>
2022-06-24 21:48:24 +0300

    Create 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,41 +1,21 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - prints the number of arguments passed into the program
- *
- *   * @argc: int
- *
- *    * @argv: list
- *
- *     * Return: 0
- *
- *      */
-
-
+ * main - prints the number of arguments passed into the program
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
 
 int main(int argc, char const *argv[])
-
 {
+int i = 0;
 
-	int i = 0;
+while (argc--)
+{
+	printf("%s\n", argv[i]);
+	i++;
+}
 
-
-
-	while (argc--)
-
-	{
-
-			printf("%s\n", argv[i]);
-
-				i++;
-
-	}
-
-
-
-	return (0);
-
+return (0);
 }
