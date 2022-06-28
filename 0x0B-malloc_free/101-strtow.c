fa94959c7a867621d86b5539b187f622e7259bce
yikunoamlak <105526101+yikunoamlak@users.noreply.github.com>
2022-06-28 22:25:17 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,223 +1,112 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * _strlen - find length of a string
- *
- *   * @s: string
- *
- *    * Return: int
- *
- *     */
-
-
-
+ * _strlen - find length of a string
+ * @s: string
+ * Return: int
+ */
 
 
 int _strlen(char *s)
-
 {
-
-	int size = 0;
-
-	for (; s[size] != '\0'; size++)
-
-		;
-
-	return (size);
-
+int size = 0;
+for (; s[size] != '\0'; size++)
+;
+return (size);
 }
 
-
-
 /**
- *
- *  * *str_concat - concatenates two strings
- *
- *   * @s1: string 1
- *
- *    * @s2: string 2
- *
- *     * Return: pointer
- *
- *      */
-
-
+ * *str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer
+ */
 
 char *str_addChar (char *str, char c)
-
 {
+int size, i;
+char *m;
 
-	int size, i;
+size = _strlen(str);
 
-	char *m;
+m = malloc((size + 1) * sizeof(char) + 1);
+if (m == 0)
+	return (0);
 
+for (i = 0; i <= size; i++)
+	m[i] = str[i];
 
+m[i + 1] = c;
+m[i + 2] = '\0';
 
-	size = _strlen(str);
-
-
-
-	m = malloc((size + 1) * sizeof(char) + 1);
-
-	if (m == 0)
-
-			return (0);
-
-
-
-	for (i = 0; i <= size; i++)
-
-			m[i] = str[i];
-
-
-
-	m[i + 1] = c;
-
-	m[i + 2] = '\0';
-
-
-
-	return (m);
-
+return (m);
 }
 
 
-
-
-
 /**
- *
- *  * *nbr_spaces - return the number of occurent of a string
- *
- *   * @s: string to check
- *
- *    * Return: int
- *
- *     */
-
-
+ * *nbr_spaces - return the number of occurent of a string
+ * @s: string to check
+ * Return: int
+ */
 
 unsigned int nbr_spaces(char *s)
-
 {
+	int i, cmpt = 0;
 
-		int i, cmpt = 0;
+	for (i = 0; s[i + 1] != '\0'; i++)
+	{
+		if (s[i]  == ' ' && s[i + 1] != ' ')
+			cmpt++;
+	}
 
-
-
-			for (i = 0; s[i + 1] != '\0'; i++)
-
-					{
-
-								if (s[i]  == ' ' && s[i + 1] != ' ')
-
-												cmpt++;
-
-									}
-
-
-
-				return (cmpt + 1);
-
+	return (cmpt + 1);
 }
 
 
-
-
-
 /**
- *
- *   *strtow - split a sentence into multiple words.
- *
- *     *@str: the string passed as argument.
- *
- *       *Return: tokens
- *
- *         */
-
+  *strtow - split a sentence into multiple words.
+  *@str: the string passed as argument.
+  *Return: tokens
+  */
 char **strtow(char *str)
-
 {
+int i;
+int spaces = nbr_spaces(str);
+char **tokens = NULL;//malloc(sizeof(char *) * (spaces));
+char *token;
+int checkingSpace = 0;
+int word = 0;
 
-	int i;
+if (!tokens)
+{
+	printf("Failed");
+	return (0);
+}
+	
 
-	int spaces = nbr_spaces(str);
-
-	char **tokens = NULL;//malloc(sizeof(char *) * (spaces));
-
-	char *token;
-
-	int checkingSpace = 0;
-
-	int word = 0;
-
-
-
-	if (!tokens)
-
+printf("looping");
+for (i = 0; str[i] != '\0'; i++)
+{
+	if (str[i] == ' ')
 	{
-
-			printf("Failed");
-
-				return (0);
-
+		if (checkingSpace == 0)
+		{
+			word++;
+			checkingSpace = 1;
+		} 
+	}
+	else
+	{
+		printf("1");
+		token = tokens[word];
+		free(tokens[word]);
+		str_addChar(token, str[i]);
+		checkingSpace = 0;
 	}
 
-		
+}
 
+tokens[i] = NULL;
 
-
-	printf("looping");
-
-	for (i = 0; str[i] != '\0'; i++)
-
-	{
-
-			if (str[i] == ' ')
-
-					{
-
-								if (checkingSpace == 0)
-
-											{
-
-															word++;
-
-																		checkingSpace = 1;
-
-																				} 
-
-									}
-
-				else
-
-						{
-
-									printf("1");
-
-											token = tokens[word];
-
-													free(tokens[word]);
-
-															str_addChar(token, str[i]);
-
-																	checkingSpace = 0;
-
-																		}
-
-
-
-	}
-
-
-
-	tokens[i] = NULL;
-
-
-
-	return (tokens);
-
+return (tokens);
 }
