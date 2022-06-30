d0390b6e62dff7974f3477c8304fc2952671472d
yikunoamlak <105526101+yikunoamlak@users.noreply.github.com>
2022-06-30 23:47:52 +0300

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,55 +1,27 @@
 #include "main.h"
-
-
-
 /**
- *
- *  * *array_range -  create an array of integers
- *
- *   * @min: size of the array
- *
- *    * @max: char to initialize
- *
- *     * Return: pointer to the array initialized or NULL
- *
- *      */
-
-
+ * *array_range -  create an array of integers
+ * @min: size of the array
+ * @max: char to initialize
+ * Return: pointer to the array initialized or NULL
+ */
 
 int *array_range(int min, int max)
-
 {
+int *m;
+int i;
 
-	int *m;
+if (min > max)
+	return (NULL);
 
-	int i;
+m = malloc((max - min + 1) * sizeof(int));
 
-
-
-	if (min > max)
-
-			return (NULL);
-
-
-
-	m = malloc((max - min + 1) * sizeof(int));
+if (m == 0)
+	return (NULL);
 
 
+for (i = 0; min + i <= max; i++)
+	m[i] = min + i;
 
-	if (m == 0)
-
-			return (NULL);
-
-
-
-
-
-	for (i = 0; min + i <= max; i++)
-
-			m[i] = min + i;
-
-
-
-	return (m);
-
+return (m);
 }
